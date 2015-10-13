10713d2dcb010b5798cbfe9d20fc39072520cbdd
8n1 <zollfrei@hotmail.com>
2015-10-13 15:48:57 +0200

    update

--- Firmware/ATtiny Arduino Sketch/tiny-ulp-controller/tiny-ulp-controller.ino
+++ Firmware/ATtiny Arduino Sketch/tiny-ulp-controller/tiny-ulp-controller.ino
@@ -19,7 +19,7 @@
 +  TIMER Reset   +-+ PB3     PB2 +-+  VREG Enable (OUTPUT)    
 +  (INPUT)         |             |                     
 +                  |             |                     
-+  VREG Shdn     +-+ PB4     PB1 +-+                   
++  VREG Shdn     +-+ PB4     PB1 +-+  WAKEUP Switch State (OUTPUT)
 +  (INPUT)         |             |                     
 +                  |             |                     
 +  GND           +-+ GND     PB0 +-+  WAKEUP Switch (INPUT)       
@@ -27,31 +27,35 @@
 +  
 +  Pin Funktionen:
 +  PB0 - WAKEUP Switch   - Taster oder Schalter über den der ATTiny aus dem Tiefschlaf geweckt wird
++  PB1 - WAKEUP Switch State - Zustand des Schalters, ermöglicht das abschalten des Spannungsteilers der für die Pegelwandlung benötigt wird
 +  PB2 - VREG Enable     - Geht auf den (active high) Enable Eingang des Spannungsreglers
 +  PB3 - TIMER Reset     - Ein kurzes HIGH Signal an diesem Pin setzt den Timer Interval zurück
 +  PB4 - VREG Shdn       - Ein kurzes HIGH Signal an diesem Pin schaltet den Spannungsreglers ab
 +
 +  Ablauf:
 +  -> Der ATtiny wird durch einen PinChange(PC) Interrupt (Wechsel sowohl von HIGH nach LOW als auch umgekehrt möglich) am Pin PB0 geweckt.
-+  -> Der Tiny setht PB2 auf HIGH und aktiviert dadurch den Spannungsregler.
-+  -> Jetzt wird gewartet bis vom ESP ein Signal über PB4 kommt dass den tiny veranlasst den Spannungsregler wieder abzuschalten,
++  -> Der Tiny setht PB2 auf HIGH und aktiviert dadurch den Spannungsregler. 
++  -> Anschließend wird der Zustand des Schalters ausgelesen und wieder über PB1 ausgegeben.
++  -> Dann wird gewartet bis vom ESP ein Signal über PB4 kommt dass den tiny veranlasst den Spannungsregler wieder abzuschalten,
 +     oder über PB3 ein Signal kommt dass (dem ESP) nochmal "timerInterval" Sekunden Zeit lässt.
 +  -> Passiert beides nicht wird der Spannungsregler um Strom zu sparen automatisch nach "timerInterval" Sekunden wieder abgeschaltet -> PB2 geht auf LOW
-+  -> Anschließend geht der ATTiny wieder in den Tiefschlaf(Power-down) und wartet auf den nächsten Interrupt.
++     Zusätzlich wird auch der Pegelwandler abgeschaltet -> PB1 wird auf LOW gesetzt.
++  -> Zu guter letzt geht der ATTiny wieder in den Tiefschlaf(Power-down) und wartet auf den nächsten Interrupt.
 +
 **************************************************************************************************************/
 
 #include <avr/sleep.h>
 
 //++++++++++++++++++++++++++++++++++++++++
-// Konfiguration
+// Pin Konfiguration
 //++++++++++++++++++++++++++++++++++++++++
 const int WAKEUP_SWITCH = 0;
-
+const int WAKEUP_SWITCH_STATE = 1;
 const int VREG_ENABLE = 2;
 const int TIMER_RESET = 3;
 const int VREG_SHTDN = 4;
 
+// Zeit in Sekunden bevor der Spannungsregler automatisch abgeschaltet wird
 const unsigned long timerInterval = 60;
 //++++++++++++++++++++++++++++++++++++++++
 
@@ -68,10 +72,11 @@ void setup()
 {
   // GPIOs konfigurieren
   pinMode(VREG_ENABLE, OUTPUT);
+  pinMode(WAKEUP_SWITCH_STATE, OUTPUT);
   pinMode(TIMER_RESET, INPUT);
   pinMode(VREG_SHTDN, INPUT);
   pinMode(WAKEUP_SWITCH, INPUT);
-  digitalWrite(WAKEUP_SWITCH, HIGH);
+  //digitalWrite(WAKEUP_SWITCH, HIGH);
   
   // AD Wandler abschalten (benötigt nur unnötig Strom)
   ADCSRA &= ~(1<<ADEN);
@@ -92,6 +97,8 @@ void loop()
   {
     // Spannungsregler abschalten
     digitalWrite(VREG_ENABLE, LOW);
+    // Spannungsteiler abschalten
+    digitalWrite(WAKEUP_SWITCH_STATE, LOW);
 
     //++++++++++++++++++++++++++++++++++++++++
     // (Nur) auf PC Interrupt an PB0 reagieren
@@ -114,7 +121,12 @@ void loop()
     // Schlafen gehen verbieten
     allow_sleep = false;
   }
-
+  
+  //++++++++++++++++++++++++++++++++++++++++
+  // Zustand des Schalter(PB0) über PB1 ausgeben
+  //++++++++++++++++++++++++++++++++++++++++
+  digitalWrite(WAKEUP_SWITCH_STATE, digitalRead(WAKEUP_SWITCH));
+    
   //++++++++++++++++++++++++++++++++++++++++
   // Maximal verfügbare Zeit abgelaufen
   //++++++++++++++++++++++++++++++++++++++++
